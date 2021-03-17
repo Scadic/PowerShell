0189a9b9cfb6e9918371b28db4803cbc4549b908
unknown <Administrator@HVIKTDEV.LOCAL>
2021-03-17 10:16:22 +0100

    Added a batch-script to set timezone on a PDC server in a Domain.

---
+++ Set-PDCNTIP.cmd
@@ -0,0 +1,13 @@
+@ECHO OFF
+echo Setup PDC TimeZone and Remote NTP Server
+tzutil /l
+echo.
+tzutil /g
+echo Select a timezone from the ones listed above: 
+set /p tz=
+tzutil /s "%tz%"
+w32tm.exe /config /syncfromflags:manual /manualpeerlist:"ntp-public.uit.no,0x8 0.no.pool.ntp.org,0x8 1.no.pool.ntp.org,0x8 2.no.pool.ntp.org,0x8 3.no.pool.ntp.org,0x8" /reliable:yes /update
+w32tm /resync /rediscover
+net stop w32time && net start w32time
+w32tm /query /status
+@ECHO ON
