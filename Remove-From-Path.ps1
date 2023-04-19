ee90e45538dc3dc72386d024cd71bdc133acab6b
Skadberg <truska@ihelse.net>
2023-04-19 12:59:10 +0200

    Removed Remove-From-Path.ps1

--- Remove-From-Path.ps1
+++ Remove-From-Path.ps1
@@ -1,10 +1,11 @@
 $OldPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'Path').Path
 ($OldPath).Split(';')
 $Rem = Read-Host -Prompt 'What element of PATH do you want to remove?'
-while (-Not $Rem)
+while (-Not $Rem -Or (-Not $OldPath.Split(';').Contains($Rem)))
 {
-	$Rem = ';'+(Read-Host -Prompt 'What element of PATH do you want to remove?')
+	$Rem = Read-Host -Prompt 'What element of PATH do you want to remove?'
 }
+$Rem = ';' + $Rem
 $NewPath = $OldPath.Replace($Rem, '')
 $Result = Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'Path' -Value $NewPath -PassThru
 ($Result.Path).Split(';')
