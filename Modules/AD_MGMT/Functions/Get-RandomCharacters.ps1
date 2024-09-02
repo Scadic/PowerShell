9986cac5cd5f4fb66f7da94158fb44d418ff17cf
Scadic <trulshskadberg@gmail.com>
2024-09-02 09:03:19 +0200

    Added password generation functions.

---
+++ Modules/AD_MGMT/Functions/Get-RandomCharacters.ps1
@@ -0,0 +1,73 @@
+﻿Function Get-RandomCharacters
+{
+
+    <#
+
+        .SYNOPSIS
+        Generate a random string.
+
+        .DESCRIPTION
+        Uses a selection of characters from a string to generate a random string/password.
+
+        .PARAMETER $Length
+        Length of the string/password.
+
+        .PARAMETER $Characthers
+        Optional: A string of all the characters available in the selection.
+
+        .INPUTS
+        An integer for the number of characters in the string/password and a string of all the characters to be available in the selection.
+
+        .OUTPUTS
+        A string of the length specified.
+
+        .EXAMPLE
+        Get-RandomCharacters -Length 20
+
+        Generate a string of length 20.
+
+    #>
+
+    [CmdletBinding()]
+    Param
+    (
+
+        [Parameter(
+            Mandatory = $False,
+            HelpMessage = "NumberOfCharacters",
+            Position = 0,
+            ValueFromPipeline = $True
+            )
+        ]
+        [System.Int64] $Length = 16,
+
+        [Parameter(
+            Mandatory = $False,
+            HelpMessage = "Which characters to use.",
+            Position = 1,
+            ValueFromPipeline = $False
+        )
+        ]
+        [System.String] $Characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[]{}\|"/?.,<>;:'
+
+    )
+
+    Begin
+    {
+
+    }
+
+    Process
+    {
+
+    }
+
+    End
+    {
+        $Random = 1..$Length | ForEach-Object {
+            Get-Random -Maximum $Characters.Length
+        }
+        $Private:ofs=""
+        Return [String]$Characters[$Random]
+    }
+}
---
+++ Modules/AD_MGMT/Functions/Get-RandomPassword.ps1
@@ -0,0 +1,86 @@
+﻿Function Get-RandomPassword
+{
+
+    <#
+
+        .SYNOPSIS
+        Generate a random password.
+
+        .DESCRIPTION
+        Uses a random word api hosted on heroku to generate a password.
+
+        .PARAMETER $NumberOfWords
+        Optional: Number of words to be requested for the password.
+
+        .PARAMETER $Simplify
+        Optional: Simplify the password to reduce the number of special character in the selection.
+
+        .INPUTS
+        An integer for the number of words in the password and a Switch (optional) to simplfy the password.
+
+        .OUTPUTS
+        A string of the generated password.
+
+        .EXAMPLE
+        Get-RandomPassword -NumberOfWords 2 -Simplify
+
+        Generate a simple password of 2 words.
+
+    #>
+
+    [CmdletBinding()]
+    Param
+    (
+
+        [Parameter(
+            Mandatory = $False,
+            HelpMessage = "Number of words for the password.",
+            Position = 0,
+            ValueFromPipeline = $True
+            )
+        ]
+        [System.Int64] $NumberOfWords = 3,
+        [Parameter(
+            Mandatory = $False,
+            HelpMessage = "Simplify password.",
+            Position = 1,
+            ValueFromPipeline = $False
+            )
+        ]
+        [Switch] $Simplify
+
+    )
+
+    Begin
+    {
+        $ApiUrl = "https://random-word-api.herokuapp.com/word?number=$($NumberOfWords)"
+    }
+
+    Process
+    {
+
+    }
+
+    End
+    {
+
+        $Words = (Invoke-WebRequest -Uri $ApiUrl | Select-Object -ExpandProperty Content).Replace('[','').Replace(']','').Replace('"','') -Split ','
+        $Words = [System.Collections.ArrayList] $Words
+        If ($Simplify){$Special = ".!()"}
+        Else {$Special = ",./<>?!@#$%^&*()=+;:'`"[]{}\|"}
+        For ($I = 0; $I -Lt $Words.Count; $I++)
+        {
+
+            $Wrd = $Words[$I]
+            $RIndex = Get-Random -Minimum 0 -Maximum ($Wrd.Length - 1)
+            $Wrd = $Wrd.Replace( $Wrd[$RIndex], ([System.String] $Wrd[$RIndex]).ToUpper() ) + [System.String] ($Special[(Get-Random -Minimum 0 -Maximum ($Special.Length-1))])
+            $Words[$I] = $Wrd
+
+        }
+        $Words = $Words -Join "$(Get-Random -Minimum 0 -Maximum 99)-"
+        $Words += "_$(Get-Random -Minimum 0 -Maximum 99)"
+
+        Return $Words
+
+    }
+}
